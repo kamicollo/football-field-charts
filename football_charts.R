90bf37ca39292b9bce9e680d856d359a7276c0fd
Aurimas R <mail@aurimas.eu>
2021-05-03 22:17:58 +0200

    Adding data files

--- football_charts.R
+++ football_charts.R
@@ -2,48 +2,78 @@ library(tidyverse)
 library(data.table)
 library(RColorBrewer)
 
-locations = fread("~/coding/real-estate/locations.csv")
-customers = fread("~/coding/real-estate/customers.csv")
-contracts = fread("~/coding/real-estate/contracts.csv")
+#read in the table that contains individual contract terms with associated start/end dates
 terms = fread("~/coding/real-estate/terms.csv")
+
+#read in the table that contains monthly information for each term (SQM / rent income in a month)
+#add proper year/month columns to the dataframe
 PL = fread("~/coding/real-estate/monthly_data.csv") %>% 
   mutate(date = as.Date(strptime(paste0(month,"-01"), format="%Y-%m-%d"))) %>%
   mutate(year = year(date), month = month(date))
 
-monthly = PL %>%
-  group_by(date) %>% 
-  summarize(rent = sum(rent), sqm = sum(sqm) * sum(effective_days) / sum(days_in_month), .groups="drop_last") %>%
-  mutate(rent_per_sqm = rent / sqm)
-  
-yearly = PL %>%
-  group_by(year) %>% 
-    summarize(rent = sum(rent), sqm = sum(sqm) * sum(effective_days) / sum(days_in_month), .groups="drop_last") %>%
-    mutate(rent_per_sqm = rent / sqm)
-
-monthly %>%  ggplot() + geom_line(aes(x = date, y=rent_per_sqm))
-yearly %>%  ggplot() + geom_line(aes(x = year, y=rent_per_sqm))  + scale_y_continuous(sec.axis = ~ .*100000) + geom_col(aes(x=year, y=sqm/100000))
-
-future_value = PL %>% filter(date > "2020-12-31") %>% group_by(term_id) %>% summarize(rent = sum(rent), .groups='drop_last') %>%
+#calculate future value per contract (assuming we're at the end of 2020)
+# filter it down to top10 contracts only
+top_10_contracts = PL %>% filter(date > "2020-12-31") %>% 
   inner_join(terms, by='term_id') %>%
-  inner_join(contracts, by="contract_id") %>% group_by(contract_id) %>% summarize(future_value = sum(rent), .groups='drop_last')
+  inner_join(contracts, by="contract_id") %>% 
+  group_by(contract_id) %>% summarize(future_value = sum(rent), .groups='drop_last') %>% 
+  arrange(desc(future_value)) %>% head(10)
 
-top_10_contracts = future_value %>% arrange(desc(future_value)) %>% head(10)
+chart_df = 
+  top_10_contracts[1,] %>% #filter to 1 contract
+  inner_join(terms, by="contract_id") %>% #get terms of top 10 contracts
+  mutate(width = as.numeric(difftime(end, start, units="days"))) %>% #calculate # of days of each term
+  mutate(mid_point = start + width/2) %>% #calculate the mid-point of each term
+  mutate(extension = as.factor(extension)) #extensions are numeric, make sure they are treated as discrete variables
 
-for_chart = terms %>%
-  inner_join(top_10_contracts, by="contract_id") %>%
-  mutate(width = as.numeric(difftime(end, start, units="days"))) %>% mutate(c = start + width/2) %>%
-  mutate(extension = as.factor(extension)) %>%
-  mutate(contract_id = reorder(as.character(contract_id), future_value, mean))
+ggplot(chart_df) + 
+  geom_tile(aes(x=mid_point, y=contract_id, width=width, height=monthly_rent, fill=extension))
 
-labels = for_chart %>% group_by(contract_id) %>% 
-  summarize(end_date = max(end), value = max(future_value), .groups="drop_last") %>%
-  mutate(label = paste0(round(value / 1000000, 1), "M"))
+chart_df = 
+  top_10_contracts %>% #all 10 contracts
+  inner_join(terms, by="contract_id") %>% #get terms of top 10 contracts
+  mutate(width = as.numeric(difftime(end, start, units="days"))) %>% #calculate # of days of each term
+  mutate(mid_point = start + width/2) %>% #calculate the mid-point of each term
+  mutate(extension = as.factor(extension)) #extensions are numeric, make sure they are treated as discrete variables
 
-for_chart %>%
-ggplot() + geom_tile(aes(x=c, y=contract_id, width=width, height=monthly_rent/max(monthly_rent), fill=extension)) + 
+ggplot(chart_df) + 
+  geom_tile(aes(x=mid_point, y=contract_id, width=width, height=monthly_rent, fill=extension))
+  
+
+chart_df = 
+  top_10_contracts %>%
+  inner_join(terms, by="contract_id") %>% #get terms of top 10 contracts
+  mutate(width = as.numeric(difftime(end, start, units="days"))) %>% #calculate # of days of each term
+  mutate(mid_point = start + width/2) %>% #calculate the mid-point of each term
+  mutate(extension = as.factor(extension)) #extensions are numeric, make sure they are treated as discrete variables
+
+ggplot(chart_df) + 
+  geom_tile(aes(x=mid_point, y=contract_id, width=width, height=monthly_rent/max(monthly_rent), fill=extension)) +
   ylab("") + xlab("Year") + theme_bw() + 
+  theme(panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank())
+  
+chart_df = 
+  top_10_contracts %>%
+  inner_join(terms, by="contract_id") %>% #get terms of top 10 contracts
+  mutate(width = as.numeric(difftime(end, start, units="days"))) %>% #calculate # of days of each term
+  mutate(mid_point = start + width/2) %>% #calculate the mid-point of each term
+  mutate(extension = as.factor(extension)) %>% #extensions are numeric, make sure they are treated as discrete variables
+  mutate(contract_id = reorder(as.character(contract_id), future_value, mean)) #reorder contracts
+
+labels = chart_df %>% group_by(contract_id) %>% #group by contract
+  summarize(end_date = max(end), value = max(future_value), .groups="drop_last") %>% #calculate total value and get end date
+  mutate(label = paste0(round(value / 1000000, 1), "M")) #format total value
+
+ggplot(chart_df) + 
+  #create rectangle geoms
+  geom_tile(aes(x=mid_point, y=contract_id, width=width, height=monthly_rent/max(monthly_rent), fill=extension)) + 
+  #deal with axis labels
+  ylab("") + xlab("Year") + theme_bw() + 
+  #remove gridlines on Y-axis
   theme(panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank()) +
-  geom_label(data = labels, mapping=aes(end_date + 400, contract_id, label=label)) + 
+  #add labels at the end of each contract bar
+  geom_label(data = labels, mapping=aes(end_date + 400, contract_id, label=label)) +
+  #apply a better color palette
   scale_fill_brewer(palette = "RdYlGn")
 
 
